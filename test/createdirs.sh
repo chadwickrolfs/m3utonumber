a1f59e6975ecf36611b3a2bb608bed34e4ba5e15
chadwick <chadwick.employment@gmail.com>
2020-12-24 17:46:19 +0100

    update create test dirs

--- test/createdirs.sh
+++ test/createdirs.sh
@@ -1,11 +1,10 @@
 M3USDIR=m3u
 
-mkdir -pv '$M3USDIR/treepeople/actual_reenactment'
-mkdir -pv '$M3USDIR/steelydanwalterbeckerdonaldfag/adecadeofsteelydanmcad11553'
-mkdir -pv '$M3USDIR/astrud_gilberto'
-mkdir -pv '$M3USDIR/johnny_cash/american_recordings'
-mkdir -pv '$M3USDIR/built_to_spill/ancient_melodies_of_the_future'
-mkdir -pv '$M3USDIR/antibalas'
-mkdir -pv '$M3USDIR/acdc'
-mkdir -pv '$M3USDIR/antal_dorati_london_symphony_orchestra'
-mkdir -pv '$M3USDIR/art_blakey_and_the_jazz_messen/a_night_in_tunisia'
+mkdir -pv $M3USDIR/treepeople/actual_reenactment
+mkdir -pv $M3USDIR/steelydanwalterbeckerdonaldfag/adecadeofsteelydanmcad11553
+mkdir -pv $M3USDIR/astrud_gilberto/verve_jazz_masters_9
+mkdir -pv $M3USDIR/johnny_cash/american_recordings
+mkdir -pv $M3USDIR/built_to_spill/ancient_melodies_of_the_future
+mkdir -pv $M3USDIR/antibalas/who_is_this_america
+mkdir -pv $M3USDIR/antal_dorati_london_symphony_orchestra/enesco__liszt_roumanian__hungarian_rhapsodies
+mkdir -pv $M3USDIR/art_blakey_and_the_jazz_messen/a_night_in_tunisia
