49f0c89890f1f0ba00272f9cd5101a14ee641a25
chadwick <chadwick.employment@gmail.com>
2020-12-25 12:04:09 +0100

    using click

---
+++ src/m3utonumber.py
@@ -0,0 +1,11 @@
+import click
+from numberfiles import NumberFiles
+
+@click.command()
+@click.argument('src', type=click.Path(exists=True), nargs=1)
+def m3utonumber(src):
+    nf = NumberFiles(src)
+    nf.numberfiles()
+
+if __name__ == '__main__':
+    m3utonumber()
