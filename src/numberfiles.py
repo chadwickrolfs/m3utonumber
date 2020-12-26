1aa030f4e49a3b3fa57d73360af8e2e4e4d62f14
chadwick <chadwick.employment@gmail.com>
2020-12-26 18:13:32 +0100

    classifying the actions, creating more robustness with try except possibilities

--- src/numberfiles.py
+++ src/numberfiles.py
@@ -14,62 +14,72 @@ class NumberFiles(object):
         self.dirtowalk = Path(dirtowalk)
 
     def numberfiles(self):
-        m3uglob = '*.m3u'
-        m3ufiles = sorted(self.dirtowalk.rglob(m3uglob))
-        for m3ufile in m3ufiles:
-            print(f'\nDEBUG:\nm3ufile: {m3ufile}\n')
-            m3udir, m3ufilename = ospath.split(m3ufile)
-            # print(f'\nDEBUG:\nm3udir: {m3udir}{os.sep}\ndirtowalk: {self.dirtowalk}\n')
-            if f'{m3udir}{os.sep}' == self.dirtowalk:
+        m3ufiles = self.get_m3ufiles()
+
+        for self.m3ufile in m3ufiles:
+            print(f'\nDEBUG:\nm3ufile: {self.m3ufile}\n')
+            self.m3udir, self.m3ufilename = ospath.split(self.m3ufile)
+            if f'{self.m3udir}{os.sep}' == self.dirtowalk:
                 continue
-            with open(m3ufile) as m3ufile_h:
-                oggtracks = m3ufile_h.readlines()
-            line0 = oggtracks[0]
+
+            self.oggtracks = self.get_oggtracks()
+            if not self.oggtracks:
+                continue
+
+            line0 = self.oggtracks[0]
             if (line0.startswith('#') or line0.startswith('<')):
-                os.remove(m3ufile)
+                os.remove(self.m3ufile)
                 continue
 
-            oggs = []
-            oggdir = set()
-            # os.chdir(m3udir)
-            # print(f'\nDEBUG:\nchdir: {os.getcwd()}')
-            for tracknumber, oggfile in enumerate(oggtracks):
-                oggfilename = ospath.basename(oggfile).strip()
-                oggglob = f'*{oggfilename}'
-                print(f'\nDEBUG:\noggfile: {oggfile}\nglob: {oggglob}')
-                oggglobbed = sorted(self.dirtowalk.rglob(oggglob))
+            try:
+                self.ogg_tracks()
+            except IndexError as e:
+                print(f'\nHANDMATIG INTERVENTIE VERREIST\n{e}')
+                continue
 
-                # possible shortened actual oggname compared to m3u
-                # examples:
-                #   acdc: what_do_you_do_for_money_h
-                #   beatles revolve: got_to_get_you_into_my_lif
-                # rename accordingly, but then how to find
-                # perhaps another glob oggfilename[0-5]*.ogg or something?
+    def get_oggtracks(self):
+        with open(self.m3ufile) as m3ufile_h:
+            return m3ufile_h.readlines()
 
-                # beastieboys check your head m3u does not reflect reality
-                # perhaps best to log these for handmatig correctie
-                # but how to get out of this double for loop ?
-                # will have to make a new method
+    def get_m3ufiles(self):
+        m3uglob = '*.m3u'
+        return sorted(self.dirtowalk.rglob(m3uglob))
 
-                oggfile = oggglobbed[0]
+    def ogg_tracks(self):
+        self.oggs = []
+        self.oggdir = set()
+        for self.tracknumber, self.oggfile in enumerate(self.oggtracks):
+            oggglobbed = self.get_oggglob()
 
-                oggdirname = ospath.dirname(oggfile)
-                oggdir.add(oggdirname)
+            self.oggfile = oggglobbed[0]
 
-                if oggfilename[0:2].isdigit():
-                    oggs.append(f'{oggfilename}\n')
-                else:
-                    numberedogg = f'{tracknumber:#02}-{oggfilename}'
-                    oggs.append(f'{numberedogg}\n')
-                    shmove(
-                            oggfile,
-                            ospath.join(oggdirname, numberedogg))
+            self.oggdirname = ospath.dirname(self.oggfile)
+            self.oggdir.add(self.oggdirname)
 
-            with open(m3ufile, 'w') as m3ufile_h:
-                m3ufile_h.writelines(oggs)
+            if self.oggfilename[0:2].isdigit():
+                self.oggs.append(f'{self.oggfilename}\n')
+            else:
+                self.move_oggfile()
 
-            if oggdir:
-                # print(f'\nDEBUG\noggdir set: {oggdir}\n')
-                oggdir = oggdir.pop()
-                if m3udir != oggdir:
-                    shmove(m3ufile, os.path.join(oggdir, m3ufilename))
+        self.write_m3ufile()
+        self.move_m3ufile()
+
+    def get_oggglob(self):
+        self.oggfilename = ospath.basename(self.oggfile).strip()
+        oggglob = f'*{self.oggfilename}'
+        return sorted(self.dirtowalk.rglob(oggglob))
+
+    def move_oggfile(self):
+        numberedogg = f'{self.tracknumber:#02}-{self.oggfilename}'
+        self.oggs.append(f'{numberedogg}\n')
+        shmove(self.oggfile, ospath.join(self.oggdirname, numberedogg))
+
+    def write_m3ufile(self):
+        with open(self.m3ufile, 'w') as m3ufile_h:
+            m3ufile_h.writelines(self.oggs)
+
+    def move_m3ufile(self):
+        if self.oggdir:
+            oggdir = self.oggdir.pop()
+            if self.m3udir != oggdir:
+                shmove(self.m3ufile, os.path.join(oggdir, self.m3ufilename))
