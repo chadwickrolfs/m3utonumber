613f101b4d0c2b4d67d8ed6a07cb8628d01096fe
chadwick <chadwick.employment@gmail.com>
2020-12-26 12:32:09 +0100

    using glob to find the m3u files

--- src/numberfiles.py
+++ src/numberfiles.py
@@ -14,13 +14,7 @@ class NumberFiles(object):
         self.dirtowalk = dirtowalk
 
     def numberfiles(self):
-        dirdepth = 0
         for dirpath, dnames, filenames in oswalk(self.dirtowalk):
-            dirdepth += 1
-            print(
-                    "\n#current depth#\n{3}\n#current path#\n{0}\
-                            \n#current directories#\n{1}\n#current files#\n{2}".format(
-                        dirpath, dnames, filenames, dirdepth))
             for filename in filenames:
                 if filename.endswith('m3u'):
                     print("found a playlist - contents:\n")
@@ -43,7 +37,9 @@ class NumberFiles(object):
                         shmove(exifile, newfile)
 
 # find m3u files
-# find ogg files
-# read m3u file line by line
+# g = glob.glob(f'{dirtowalk}/**/*.m3u', recursive=True)
+# readlines from m3u file, only the filename
+#     with open(g[0], 'r+') as gf
+#       oggs = [os.path.basename(x) for x in gf.readlines()]
 #     compare with each ogg file
 #     rename ogg file with number
