571ce052278f0440f1978f305f3e54748c56e310
chadwick <chadwick.employment@gmail.com>
2020-12-26 17:45:14 +0100

    using pathlib with Path.rglob and debugging

--- src/numberfiles.py
+++ src/numberfiles.py
@@ -1,7 +1,7 @@
 import os
 import os.path as ospath
 from shutil import move as shmove
-from glob import glob
+from pathlib import Path
 
 class NumberFiles(object):
     '''
@@ -11,30 +11,45 @@ class NumberFiles(object):
 
     def __init__(self, dirtowalk):
         '''click '''
-        self.dirtowalk = dirtowalk
+        self.dirtowalk = Path(dirtowalk)
 
     def numberfiles(self):
-        m3uglob = f'{self.dirtowalk}**/*.m3u'
-        # TODO: consider Path.rglob
-        m3ufiles = glob(m3uglob, recursive=True)
+        m3uglob = '*.m3u'
+        m3ufiles = sorted(self.dirtowalk.rglob(m3uglob))
         for m3ufile in m3ufiles:
             print(f'\nDEBUG:\nm3ufile: {m3ufile}\n')
             m3udir, m3ufilename = ospath.split(m3ufile)
             # print(f'\nDEBUG:\nm3udir: {m3udir}{os.sep}\ndirtowalk: {self.dirtowalk}\n')
             if f'{m3udir}{os.sep}' == self.dirtowalk:
                 continue
-            oggs = []
-            oggdir = set()
             with open(m3ufile) as m3ufile_h:
                 oggtracks = m3ufile_h.readlines()
+            line0 = oggtracks[0]
+            if (line0.startswith('#') or line0.startswith('<')):
+                os.remove(m3ufile)
+                continue
 
+            oggs = []
+            oggdir = set()
             # os.chdir(m3udir)
             # print(f'\nDEBUG:\nchdir: {os.getcwd()}')
             for tracknumber, oggfile in enumerate(oggtracks):
                 oggfilename = ospath.basename(oggfile).strip()
-                oggglob = f'{self.dirtowalk}**/*{oggfilename}'
-                # print(f'\nDEBUG:\noggfile: {oggfile}\nglob: {oggglob}')
-                oggglobbed = glob(oggglob, recursive=True)
+                oggglob = f'*{oggfilename}'
+                print(f'\nDEBUG:\noggfile: {oggfile}\nglob: {oggglob}')
+                oggglobbed = sorted(self.dirtowalk.rglob(oggglob))
+
+                # possible shortened actual oggname compared to m3u
+                # examples:
+                #   acdc: what_do_you_do_for_money_h
+                #   beatles revolve: got_to_get_you_into_my_lif
+                # rename accordingly, but then how to find
+                # perhaps another glob oggfilename[0-5]*.ogg or something?
+
+                # beastieboys check your head m3u does not reflect reality
+                # perhaps best to log these for handmatig correctie
+                # but how to get out of this double for loop ?
+                # will have to make a new method
 
                 oggfile = oggglobbed[0]
 
@@ -45,7 +60,7 @@ class NumberFiles(object):
                     oggs.append(f'{oggfilename}\n')
                 else:
                     numberedogg = f'{tracknumber:#02}-{oggfilename}'
-                    oggs.append(f'numberedogg\n')
+                    oggs.append(f'{numberedogg}\n')
                     shmove(
                             oggfile,
                             ospath.join(oggdirname, numberedogg))
