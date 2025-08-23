6a6415b40811f6f6dda5a9de5646e12c26f25522
Chadwick <chadwick.employment@gmail.com>
2025-08-23 14:43:34 +0200

    use dataclasses

--- src/numberfiles.py
+++ src/numberfiles.py
@@ -1,5 +1,18 @@
 import os
+from dataclasses import dataclass
 from shutil import move as shmove
+
+
+@dataclass
+class playlist:
+    m3uname: str
+    m3udir: str
+    m3ulist: list
+
+
+@dataclass
+class playfile:
+    original_path: str
 
 
 class NumberFiles(object):
@@ -18,9 +31,21 @@ class NumberFiles(object):
 
         for self.m3ufile in m3ufiles:
             print(f'\nDEBUG:\nm3ufile: {self.m3ufile}\n')
-            self.m3udir = self.m3ufile.parent
-            self.m3ufilename = self.m3ufile.name
-            if f'{self.m3udir}{os.sep}' == self.dirtowalk:
+            filelist = [
+                    playfile(listfile)
+                    for listfile
+                    in self.m3ufile.read_text.split("\n")
+            ]
+            self.playlist = playlist(
+                self.m3ufile.name,
+                self.m3ufile.parent,
+                filelist,
+            )
+
+            # not sure why this happens
+            # better to remove before the loop
+            if f'{self.playlist.m3udir}{os.sep}' == self.dirtowalk:
+                print(f"found '{self.playlist.m3udir}{os.sep}'")
                 continue
 
             self.oggtracks = self.get_oggtracks()
@@ -81,7 +106,7 @@ class NumberFiles(object):
     def move_m3ufile(self):
         if self.oggdir:
             oggdir = self.oggdir.pop()
-            if self.m3udir != oggdir:
+            if self.playlist.m3udir != oggdir:
                 shmove(
                         self.m3ufile,
-                        os.path.join(oggdir, self.m3ufilename))
+                        os.path.join(oggdir, self.playlist.m3ufilename))
